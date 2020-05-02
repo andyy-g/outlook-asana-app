a2c7394d48c195dd63365a0a64d6f2cb67490eae
Andréa Granger <granger.ndr@gmail.com>
2020-05-02 19:06:04 +0200

    daemon file modified

--- app_daemon.rb
+++ app_daemon.rb
@@ -1,6 +1,5 @@
 #!/usr/bin/env ruby
 
-require 'rubygems'
 require 'bundler'
 Bundler.require
 
@@ -10,12 +9,12 @@ pwd  = File.dirname(File.expand_path(__FILE__))
 file = pwd + '/app.rb'
 
 Daemons.run_proc(
-  'outlook_asana_app', # name of daemon
+  'outlook_asana_app' # name of daemon
   #  :dir_mode => :normal
   #  :dir => File.join(pwd, 'tmp/pids'), # directory where pid file will be stored
   #  :backtrace => true,
   #  :monitor => true,
-  :log_output => true
+  #  :log_output => true
 ) do
   exec "ruby #{file}"
 end
